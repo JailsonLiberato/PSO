4769e027a416d57a7f928811d24176cd726e6e22
Jailson Liberato <jailsonliberato@gmail.com>
2019-04-12 17:05:51 -0300

    Estrutura do algoritmo de PSO.

--- PSO/src/business/PSOService.java
+++ PSO/src/business/PSOService.java
@@ -8,14 +8,85 @@ import model.Particle;
 
 public class PSOService {
 
+	private static final int NUMBER_DIMENSIONS = 30;
+	private static final int NUMBER_PARTICLES = 30;
+	private static final int NUMBER_MAX_ITERATIONS = 10000;
+
 	private List<Particle> particles;
+	private FunctionType functionType;
+	private List<Double> gBest;
 
 	public PSOService() {
 		particles = new ArrayList<>();
+		gBest = new ArrayList<>();
+
 	}
 
 	public Function executeFunction(FunctionType functionType) {
+		this.functionType = functionType;
+		initializeParticles();
+		int countIterations = 0;
+		gBest.clear();
+		do {
+			calculateFitness();
+			updateGlobalBest();
+			calculateVelocity();
+			updatePosition();
+		} while (countIterations < NUMBER_MAX_ITERATIONS);
 		return null;
+	}
+
+	private void calculateVelocity() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void updatePosition() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void updateGlobalBest() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void calculateFitness() {
+		for (Particle particle : particles) {
+
+		}
+
+	}
+
+	private void initializeParticles() {
+		createParticles();
+	}
+
+	private Particle createParticle() {
+		Particle particle = new Particle();
+		particle = createDimensions(particle);
+		return particle;
+	}
+
+	private Particle createDimensions(Particle particle) {
+		for (int i = 0; i < NUMBER_DIMENSIONS; i++) {
+			Double position = generateRandomPosition();
+			Double pBest = new Double(position);
+			particle.getPosition().add(position);
+			particle.getPbest().add(pBest);
+		}
+		return particle;
+	}
+
+	private Double generateRandomPosition() {
+		return Math.random() * functionType.getBound();
+	}
+
+	private void createParticles() {
+		particles.clear();
+		for (int i = 0; i < NUMBER_PARTICLES; i++) {
+			particles.add(createParticle());
+		}
 	}
 
 	public List<Particle> getParticles() {
@@ -24,5 +95,13 @@ public class PSOService {
 
 	public void setParticles(List<Particle> particles) {
 		this.particles = particles;
+	}
+
+	public FunctionType getFunctionType() {
+		return functionType;
+	}
+
+	public void setFunctionType(FunctionType functionType) {
+		this.functionType = functionType;
 	}
 }
--- PSO/src/model/Particle.java
+++ PSO/src/model/Particle.java
@@ -4,36 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Particle {
-	
+
 	private List<Double> position;
 	private List<Double> velocity;
 	private List<Double> pbest;
-	
+
 	public Particle() {
 		position = new ArrayList<>();
 		velocity = new ArrayList<>();
 		pbest = new ArrayList<>();
 	}
-	
+
 	public List<Double> getPosition() {
 		return position;
 	}
+
 	public void setPosition(List<Double> position) {
 		this.position = position;
 	}
+
 	public List<Double> getVelocity() {
 		return velocity;
 	}
+
 	public void setVelocity(List<Double> velocity) {
 		this.velocity = velocity;
 	}
+
 	public List<Double> getPbest() {
 		return pbest;
 	}
+
 	public void setPbest(List<Double> pbest) {
 		this.pbest = pbest;
 	}
-	
-	
 
 }
