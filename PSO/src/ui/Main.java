4fd503af5986947c28b8f1ba241078d1120f0bab
Jailson Liberato <jailsonliberato@gmail.com>
2019-04-16 10:06:19 -0300

    Adicionando as threads.

--- PSO/src/ui/Main.java
+++ PSO/src/ui/Main.java
@@ -7,19 +7,35 @@ import model.Function;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		PSOService psoService = new PSOService();
-		Function sphereFuncion = psoService.executeFunction(FunctionType.SPHERE);
-		Function rastriginFunction = psoService.executeFunction(FunctionType.RASTRINGIN);
-		Function rosenbrockFunction = psoService.executeFunction(FunctionType.ROSENBROCK);
-		
-		JFreeChartUtil.createChart(FunctionType.SPHERE, sphereFuncion);
 
-		JFreeChartUtil.createChart(FunctionType.RASTRINGIN, rastriginFunction);
+		Thread threadSphere = new Thread() {
+			public void run() {
+				Function sphereFuncion = psoService.executeFunction(FunctionType.SPHERE);
+				JFreeChartUtil.createChart(FunctionType.SPHERE, sphereFuncion);
+			}
+		};
 
-		JFreeChartUtil.createChart(FunctionType.ROSENBROCK, rosenbrockFunction);
-		
+		threadSphere.start();
+
+		Thread threadRastrigin = new Thread() {
+			public void run() {
+				Function rastriginFunction = psoService.executeFunction(FunctionType.RASTRINGIN);
+				JFreeChartUtil.createChart(FunctionType.RASTRINGIN, rastriginFunction);
+			}
+		};
+
+		threadRastrigin.start();
+
+		Thread threadRosenbrock = new Thread() {
+			public void run() {
+				Function rosenbrockFunction = psoService.executeFunction(FunctionType.ROSENBROCK);
+				JFreeChartUtil.createChart(FunctionType.ROSENBROCK, rosenbrockFunction);
+			}
+		};
+
+		threadRosenbrock.start();
 
 	}
-
 }
